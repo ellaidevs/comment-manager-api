1f6f88038308d784d5df5d16293dcb3f3ba0a3d7
Ellai <ellaiyarasan@intrack.com.my>
2021-07-21 00:43:47 +0800

    feat: add graphql endpoint boiler code

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 const express = require('express');
-// const expressGraphQl = require('express-graphql');
 const {graphqlHTTP} = require('express-graphql');
 const app = express();
+
+const schema = require('./schema/schema');
+
+const PORT = 5000;
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
+
+app.listen(PORT, () => {
+    console.log(`listening on PORT :${PORT}`);
+});
+
