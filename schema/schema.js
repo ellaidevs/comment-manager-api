3c18af8b66505c8db5f3cd259f091e5919d618d7
Ellai <ellaiyarasan@intrack.com.my>
2021-07-21 00:48:07 +0800

    feat: add graphql PostType and CommentType

--- schema/schema.js
+++ schema/schema.js
@@ -2,3 +2,24 @@ const graphql = require('graphql');
 const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema, GraphQLList} = graphql;
 const _ = require('lodash');
 const data = require('./data');
+
+const PostType = new GraphQLObjectType({
+    name: 'Post',
+    fields: {
+        userId: {type: GraphQLInt},
+        id: {type: GraphQLInt},
+        title: {type: GraphQLString},
+        body: {type: GraphQLString}
+    }
+})
+
+const CommentType = new GraphQLObjectType({
+    name: 'Comment',
+    fields: {
+        postId: {type: GraphQLInt},
+        id: {type: GraphQLInt},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        body: {type: GraphQLString}
+    }
+})
