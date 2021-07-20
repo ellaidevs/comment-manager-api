c4030e40db60c8a3b559b6f1e10e2ecf2bc99f26
Ellai <ellaiyarasan@intrack.com.my>
2021-07-21 00:46:56 +0800

    feat: add graphql schema file

---
+++ schema/schema.js
@@ -0,0 +1,4 @@
+const graphql = require('graphql');
+const {GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLSchema, GraphQLList} = graphql;
+const _ = require('lodash');
+const data = require('./data');
